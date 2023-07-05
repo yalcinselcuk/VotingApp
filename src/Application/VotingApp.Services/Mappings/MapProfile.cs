affe081b4860a4c286251151cfc13413d1ec2092
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-07-05 23:51:45 +0300

    map'leme yapıldı ve service' e get metodu eklendi

---
+++ src/Application/VotingApp.Services/Mappings/MapProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using VotingApp.Dto.Responses;
+using VotingApp.Entities;
+
+namespace VotingApp.Services.Mappings
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile() 
+        {
+            CreateMap<Poll, PollResponse>();
+        }
+
+    }
+}
--- src/Application/VotingApp.Services/PollService.cs
+++ src/Application/VotingApp.Services/PollService.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VotingApp.Dto.Responses;
 
 namespace VotingApp.Services
 {
-    public  class PollService : IPollService
+    public class PollService : IPollService
     {
+        public IEnumerable<PollResponse> GetPollResponse()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
